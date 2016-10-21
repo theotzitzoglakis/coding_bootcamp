533f8a14e028cacddf3db5a2715df468df2ee7d7
Theodor Tzitzoglakis <theotzitzoglakis@yahoo.gr>
2016-10-21 13:40:29 +0300

    Add files via upload

---
+++ C%23%20Exercises/Demo1/Demo1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool i = true;
+            while (i == true)
+            {
+                Console.WriteLine("Give me a number between 1 and 6");
+                string NumberStr = Console.ReadLine();
+                int Number = Convert.ToInt32(NumberStr);
+                if (Number < 1 || Number > 6)
+                {
+                    Console.WriteLine("The number you entered is out of range");
+                    continue;
+                }
+
+                Random r = new Random();
+                int LuckyNumber = r.Next(1, 7);
+
+                if (LuckyNumber == Number)
+                {
+                    Console.WriteLine("You win!!!!");
+                    i=false;
+                }
+                else
+                {
+                    Console.WriteLine("You lose :( , the correct number was " + LuckyNumber);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C%23%20Exercises/Mobile/Mobile/Usage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile
+{
+    class Usage
+    {
+        public int BattPercentage { get; private set; }
+        public string OS { get; private set; }
+        public List<Call> History = new List<Call>();
+
+        public Usage()
+        {
+            BattPercentage = 100;
+            History = new List<Call>();
+        }
+
+        public void NewCall(Call.CallDirection type)
+        {
+            History.Add(new Call(type));
+        }
+
+        public void EndCall()
+        {
+            History[History.Count - 1].End();
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += "Battery" + BattPercentage + "%\n";
+            result += "Call History: \n";
+            foreach (Call call in History)
+            {
+                result+= String.Format("Start Time: {0} | Duration: {1} seconds \n", call.CallStart.ToString(), call.Duration);
+            }
+            return result;
+        }
+    }
+}
